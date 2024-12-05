cd9ebc9c437bca9468757949885fcd2b121bd506
LifeIsCo <altasan1012@gmail.com>
2024-12-05 11:50:27 +0100

    doing a few photos

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from main.models import Item
+from main.models import Item, ItemPhoto
 
 admin.site.register(Item)
+admin.site.register(ItemPhoto)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from PIL.ImageTk import PhotoImage
 from django.shortcuts import render
-from main.models import Item
+from main.models import Item, ItemPhoto
 
 def home(request):
     return render(request, 'main/index.html')
@@ -10,5 +11,14 @@ def buy(request):
 
 
 def catalog(request):
-    data = {'candles': Item.objects.all()}
+    item_ids = [num.id for num in Item.objects.all()]
+    photos_item = ItemPhoto.objects.filter(item_id__in=item_ids)
+    print(photos_item)
+    #  TODO у каждого товара выводится его фото:
+        #  TODO гуглить SQL group by
+        #  TODO гуглить Django ORM group by
+        #  TODO гуглить SQL left joint
+        #  TODO гуглить Django ORM left joint
+    data = {'candles': Item.objects.all(),
+            'photos':photos_item }
     return render(request, 'main/catalogue.html', context=data)
