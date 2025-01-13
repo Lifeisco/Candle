86fb82486f320893445f76fc7a1808bd9721b3eb
LifeIsCo <altasan1012@gmail.com>
2025-01-13 18:29:20 +0100

    new way to upload photos

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import sys
+from PIL import Image
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +13,16 @@ class Item(models.Model):
 
 class ItemPhoto(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to='img/', null=True, blank=True)
+    # thumbnail = models.ImageField(upload_to='thumbs/', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.image.name} - {self.item.name}'
+        return f'{self.item} - фото {self.image}'
+
+'''    def make_thumbnail(self):
+        output_size = (300, 600)
+        thumbnail_img = Image.open(self.image)
+        thumbnail_img.thumbnail(output_size)
+        thumbnail_img.save(self.image.name + ".thumbnail", "PNG")'''
+
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from PIL.ImageTk import PhotoImage
 from django.shortcuts import render
 from main.models import Item, ItemPhoto
+
 
 def home(request):
     return render(request, 'main/index.html')
@@ -11,14 +11,10 @@ def buy(request):
 
 
 def catalog(request):
-    item_ids = [num.id for num in Item.objects.all()]
-    photos_item = ItemPhoto.objects.filter(item_id__in=item_ids)
-    print(photos_item)
-    #  TODO у каждого товара выводится его фото:
-        #  TODO гуглить SQL group by
-        #  TODO гуглить Django ORM group by
-        #  TODO гуглить SQL left joint
-        #  TODO гуглить Django ORM left joint
-    data = {'candles': Item.objects.all(),
-            'photos':photos_item }
+    items = Item.objects.all()
+    for item in items:
+        item.img = ItemPhoto.objects.filter(item=item).values('image')
+
+    data = {'items':items}
+
     return render(request, 'main/catalogue.html', context=data)
